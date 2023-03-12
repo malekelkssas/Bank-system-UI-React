32d9aeeb60e7d63a4619fdf738cab736f38066c8
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-12 13:27:40 +0200

    starting putting the chatbot

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Welcome</h1>
             <div className="links">
-                <a href='/'>Home</a>
+                <a href='/'>Sign in</a>
                 <a href="/create" style={{
                     color:'white',
                     backgroundColor:"red",
                     borderRadius:'8px'
-                }}>new page</a>
+                }}>New acount</a>
             </div>
         </nav>
      );
