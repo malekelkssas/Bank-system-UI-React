4f39d608600d1b6da8b983489249ba1fdeb72429
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-14 17:43:26 +0200

    making the chatbot recieving massages

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Chatbot = (props) => {
-
+    let id = 0;
     const [hidden,setHidden] = useState('hidden');
+    const [formData, setFormData] = useState([{name:'Malek Mohamed',mass:'Malek Mohamed'}]);
+    const [title,setTitle] = useState('');
+
       const chatbotchat = () =>{
        if (hidden==='hidden')
         setHidden('inherit');
       else
         setHidden('hidden');
   }
+
+  const handlesubmit = (e)=>{
+    e.preventDefault();
+    let tmp = formData;
+    tmp.push({name:'user',mass:title})
+    setTitle('');
+    setFormData(tmp);
+    
+    console.log(formData,e.target.value);
+  }
+
     return (  
         <div className="algo-journal-btn" onClick={chatbotchat}>
       <div className="algo-journal-wrapper" style={{visibility:hidden}} onClick={(e) => e.stopPropagation()}>
         <div className="algo-journal-header">Malek Mohamed Elkssas</div>
           <div className="algo-journal-list" >
-            <div className="algo-journal-list-item">
-                <div className="student-name">Malek Mohamed</div>
-                <div className="project-name">Hi</div>
-            </div>
+            
+                {
+                formData.map((item) => (
+                  <div className="algo-journal-list-item" key={id++}>
+                    <div className="student-name">{item.name}</div>
+                    <div className="project-name">{item.mass}</div>
+                  </div>
+                ))
+                }
+            
           </div> 
-          <form  style={{position: 'absolute', bottom: '0',display:'flex'}}>
-            <input type="text" style={{color:'black',backgroundColor:'white',width:'300px'}}/>
+          <form onSubmit={handlesubmit} style={{position: 'absolute', bottom: '0',display:'flex'}}>
+            <input type="text" value={title} onChange={e =>setTitle(e.target.value)} style={{color:'black',backgroundColor:'white',width:'300px'}}/>
             <button style={{backgroundColor:'red'}}>.</button>
           </form>
       </div>
