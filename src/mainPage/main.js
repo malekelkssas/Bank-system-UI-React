09c05cdc5d82fbc9845a466526231e842f997556
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-05-29 14:03:07 +0200

    finishing the main page

--- src/mainPage/main.js
+++ src/mainPage/main.js
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import Button from "./components/button";
 import { useEffect } from "react";
+import Mail from '../images/mail.png';
+import Phone from '../images/phone.png';
+import Address from '../images/address.png';
+import Smile from '../images/smile.jpg';
+import BankOffine from '../images/bank-office.jpg';
 
 function Main (){
+
+  
+
 
      const buttonNames = [
     "Open Account",
@@ -18,16 +27,48 @@ function Main (){
   ];
   //TODO: "Report Issues","report credit card issue"
 
-    const renderButtons = () => {
-    return buttonNames.map((buttonName, index) => (
-      <Button key={index} buttonText={buttonName} />
-    ));
-  };
 
 return (
-    <div className="grid-container">
-        {renderButtons()}
+    <div className="main-content">
+    <h1>About GUCB</h1>
+    <div className="box">
+      <img src={Smile} alt="Image of GUCB" />
+      <p>Welcome to our bank website! We are committed to providing excellent banking services to our customers. Whether you need assistance with managing your accounts, applying for loans, or making transactions, we've got you covered. Feel free to explore our website and discover the wide range of services we offer.</p>
     </div>
+
+    <div className="box">
+      <img src={BankOffine} alt="Image of GUCB" />
+      <p>Welcome to our bank website! We are committed to providing excellent banking services to our customers. Whether you need assistance with managing your accounts, applying for loans, or making transactions, we've got you covered. Feel free to explore our website and discover the wide range of services we offer.</p>
+    </div>
+
+    <section className="cards contact" id="contact">
+      <h2 className="title">Contact Information</h2>
+      <div className="content">
+          <div className="card">
+                <div className="info">
+                  <img src={Mail} />
+                <h3> Email</h3>
+                {/* <a href="mailto:info@GUCB.com.eg"><p>info@GUCB.com.eg</p></a> */}
+                </div>
+          </div>
+          <div className="card">
+              <div className="info">
+                <img src={Phone} />
+              <h3> Phone</h3>
+              {/* <a href="tel:11942"><p>11942</p></a> */}
+              </div>
+          </div>  
+
+          <div className="card">
+            <div className="info">
+              <img src={Address} />
+            <h3>Head Office</h3>
+            <p>5th Settlement, Cairo</p>
+            </div>
+        </div> 
+          </div>
+  </section>
+  </div>
 );
 }
 export default Main;
